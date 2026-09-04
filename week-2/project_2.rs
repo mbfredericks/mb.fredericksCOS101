ffd11bafe25fde92b8434f4c3be256fcb575e41d
unknown <benjamin.fredericks@pau.edu.ng>
2026-09-04 21:12:45 +0100

    (Project 3)

---
+++ week-2/project_2.rs
@@ -0,0 +1,18 @@
+fn main () {
+	let toshiba_quantity : f64 = 2.0;
+	let toshiba_amount : f64 = 450_000.00;
+	let mac_quantity : f64 = 1.0;
+	let mac_amount : f64 = 1_500_000.00;
+	let hp_quantity : f64 = 3.0;
+	let hp_amount : f64 = 750_000.00;
+    let dell_quantity : f64 = 3.0;
+    let dell_amount : f64 = 2_850_000.00;
+    let acer_quantity : f64 = 1.0;
+    let acer_amount : f64 = 250_000.00;
+    // sum
+    let _quantity_sum = toshiba_quantity + mac_quantity + hp_quantity + dell_quantity + acer_quantity;
+    println!("Quantity sum is {:}", );
+    let _amount_sum = toshiba_amount + mac_amount + hp_amount + dell_amount + acer_amount;
+    println!("Amount sum is {:}", );
+
+}
