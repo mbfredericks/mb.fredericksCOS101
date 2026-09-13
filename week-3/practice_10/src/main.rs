58673077595f29888c7ad0470c2d497eccd3c60a
unknown <benjamin.fredericks@pau.edu.ng>
2026-09-13 14:47:24 +0100

    (Cargo Practice 10)

---
+++ week-3/practice_10/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    // addition
+    let sum = 5550 + 7310;
+    println!("The sum of 5550 and 7310 is {}", sum);
+
+    // subtraction
+    let difference:f32 = 95.5 - 4.3;
+    println!("The difference of 95.5 and 4.3 is {}", difference);
+
+    // multiplication
+    let product:i32 = 4 * 30;
+    println!("The product of 4 and 30 is {}", product);
+
+    // division
+    let quotient = 56.7 / 32.2;
+    println!("The division of 56.7 and 32.2 is {:.2}", quotient);
+
+    // remainder
+    let remainder = 43 % 5;  
+    println!("The remainder 43 and 5 is {}", remainder);
+
+}
